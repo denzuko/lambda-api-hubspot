b10cc9d9233534437f6061f8e7f990f011790021
Dwight Spencer <dspencer@computekindustries.com>
2020-09-25 03:04:39 -0500

    (feat) sqs trigger for lambda

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -92,3 +92,10 @@ resource "aws_api_gateway_method" "webhook_method_get" {
   http_method = "GET"
   authorization = "NONE"
 }
+
+resource "aws_lambda_event_source_mapping" "event_source_mapping" {
+  batch_size        = 1
+  event_source_arn  = "${aws_sqs_queue.hubspot_fifo.arn}"
+  enabled           = true
+  function_name     = "${aws_lambda_function.lambda_s3.arn}"
+}
